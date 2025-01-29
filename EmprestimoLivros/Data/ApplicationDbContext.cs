c173ea43e96647ae7409b99893e31b86f13af124
resnann <renanpjo@gmail.com>
2025-01-29 16:28:34 -0300

    crud completo

---
+++ EmprestimoLivros/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmprestimoLivros.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<EmprestimoLivros.Models.EmprestimosModel> Emprestimos { get; set; }
+
+    }
+}
